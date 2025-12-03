0d189c3a5d2621381106a34d3c426b36ea087071
mina2091 <nilsyes20@gmail.com>
2025-12-03 22:40:26 +0100

    added ap png to button + some sprites

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,14 @@ class $modify(menu, CreatorLayer) {
 		
 		auto bottomLeftMenu = this->getChildByID("bottom-left-menu");
 		auto apButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+			//CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+			CCSprite::create("archi_shadow.png"_spr),
 			this,
 			menu_selector(menu::onButtonClick)
 		);
 
-		//auto imgTest = CCSprite::create("dsdsdsd,jpg"_spr);
+		//auto imgTest = CCSprite::create("dsdsdsd.jpg"_spr);
+		//auto imgTest2 = CCSprite::create("archi.png"_spr);
 
 		bottomLeftMenu->addChild(apButton);
 
