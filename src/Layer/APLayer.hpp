35118d981aa81a51d732cca403c44758e2718c77
Jellyfishii <why.u.why1773@gmail.com>
2025-12-04 21:57:17 +0100

    jellytestbranch init

---
+++ src/Layer/APLayer.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class APLayer : public CCLayer {
+public:
+    CREATE_FUNC(APLayer);
+
+    // Öffnet die Scene mit diesem Layer
+    void show();
+
+    // Button-Handler — muss in der Header-Datei deklariert sein
+    void onButtonClick(CCObject* btn);
+
+protected:
+    bool init() override;
+};
