6dcd94b31e20aa48effc4db2528dc55d65cd4099
mina2091 <nilsyes20@gmail.com>
2025-12-04 18:41:07 +0100

    ap button click -> ap login layer

---
+++ src/Layer/APLogInLayer.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class APLogInLayer : public CCLayer {
+public:
+    // Erzeugt eine Instanz mit dem üblichen Cocos2d-x/Geode-Makro
+    CREATE_FUNC(APLogInLayer);
+
+    // Fügt dieses Layer einer neuen Szene hinzu und öffnet sie
+    void show();
+
+protected:
+    bool init() override;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/ui/TextInput.hpp>
-#include <Geode/binding/CCTextInputNode.hpp>
+#include <Geode/ui/LazySprite.hpp>
+#include "Layer/APLogInLayer.hpp"
 
 using namespace geode::prelude;
 
@@ -10,7 +10,7 @@ class $modify(menu, CreatorLayer) {
 	bool init() {
 		if (!CreatorLayer::init())
 			return false;
-		
+
 		auto bottomLeftMenu = this->getChildByID("bottom-left-menu");
 		auto apButton = CCMenuItemSpriteExtra::create(
 			//CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
@@ -19,36 +19,18 @@ class $modify(menu, CreatorLayer) {
 			menu_selector(menu::onButtonClick)
 		);
 
-		//auto imgTest = CCSprite::create("dsdsdsd.jpg"_spr);
-		//auto imgTest2 = CCSprite::create("archi.png"_spr);
+		//auto imgTest = CCSprite::create("dsdsdsd,jpg"_spr);
 
 		bottomLeftMenu->addChild(apButton);
 
 		apButton->ignoreAnchorPointForPosition(true);
 
+
 		return true;
-
-		
-
 	}
 
-	void onButtonClick(CCObject* btn) {
-		geode::createQuickPopup(
-			"Title",            // title
-			"auto apButton",   // content
-			"connect", "cancel",      // buttons
-			[](auto, bool btn2) {
-				if (!btn2) {
-					FLAlertLayer::create("gdoa", "connected", "ok")->show();
-				}
-				else {
-					FLAlertLayer::create("gdoa", "cancelled", "ok")->show();
-				}
-			}
-		);
+	void onButtonClick(CCObject * btn) {
+		APLogInLayer::create()->show();
 	}
-
-	
-
 
 };
